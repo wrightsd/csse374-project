fc20065a2bba0c93e44c8abb1095a5e37d6fe2bc
Gregory Nathan <nathangj@rose-hulman.edu>
2015-12-16 15:25:55 -0500

    Spencer Wright: Create method builder and refactor into 3 seperate builders

--- UMLLaserDinosaurs/src/problem/asm/ClassMethodVisitor.java
+++ UMLLaserDinosaurs/src/problem/asm/ClassMethodVisitor.java
@@ -3,14 +3,15 @@ package problem.asm;
 import java.util.Arrays;
 
 import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.FieldVisitor;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 
 public class ClassMethodVisitor extends ClassVisitor {
-	
+
 	private StringBuilder builder;
-	
+
 	public ClassMethodVisitor(int api) {
 		super(api);
 	}
@@ -18,7 +19,7 @@ public class ClassMethodVisitor extends ClassVisitor {
 	public ClassMethodVisitor(int api, ClassVisitor decorated) {
 		super(api, decorated);
 	}
-	
+
 	public ClassMethodVisitor(int api, ClassVisitor decorated, StringBuilder builder) {
 		super(api, decorated);
 		this.builder = builder;
@@ -28,16 +29,11 @@ public class ClassMethodVisitor extends ClassVisitor {
 	public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
 		MethodVisitor toDecorate = super.visitMethod(access, name, desc, signature, exceptions);
 		// TODO: delete the line below
-		Type[] argTypes = Type.getArgumentTypes(desc); 
+		Type[] argTypes = Type.getArgumentTypes(desc);
 		String[] classNames = new String[argTypes.length];
-		for(int i = 0; i < argTypes.length; i++){
+		for (int i = 0; i < argTypes.length; i++) {
 			classNames[i] = argTypes[i].getClassName();
 		}
-		String symbol="";
-		if((access & Opcodes.ACC_PUBLIC) != 0){
-			symbol = "+";
-		}
-		System.out.println("            method " + symbol +name + " " + Type.getReturnType(desc).getClassName() + Arrays.toString(argTypes));
 		// TODO: create an internal representation of the current method and
 		// pass it to the methods below
 		// addAccessLevel(access);
@@ -47,7 +43,21 @@ public class ClassMethodVisitor extends ClassVisitor {
 		// current class
 		// What is a good way for the code to remember what the current class
 		// is?
-		
+		String type = Type.getReturnType(desc).getClassName();
+		// TODO: delete the line below
+		if (access == Opcodes.ACC_PUBLIC) {
+			builder.append("+");
+		} else { // For now, assume that it is private if it is not public
+			builder.append("-");
+		}
+		String argsToPrint = Arrays.toString(classNames);
+		if (argsToPrint.length() > 2) {
+			argsToPrint = argsToPrint.substring(1, argsToPrint.length() - 1);
+		}
+		builder.append(name + "(" + argsToPrint + ")" + " : " + type + "\\l");
+		// TODO: add this field to your internal representation of the current
+		// class.
+		// What is a good way to know what the current class is?
 		return toDecorate;
 	}
 }
--- UMLLaserDinosaurs/src/problem/asm/DesignParser.java
+++ UMLLaserDinosaurs/src/problem/asm/DesignParser.java
@@ -11,31 +11,38 @@ public class DesignParser {
 
 	public static void main(String[] args) throws IOException {
 		for (String className : args) {
-			StringBuilder builder = new StringBuilder();
-			builder.append("digraph text{\n");
-			builder.append("rankdir=BT;\n");
+			StringBuilder completeBuilder = new StringBuilder();
+			StringBuilder methodBuilder = new StringBuilder();
+			StringBuilder fieldBuilder = new StringBuilder();
+			StringBuilder arrowBuilder = new StringBuilder();
+			completeBuilder.append("digraph text{\n");
+			completeBuilder.append("rankdir=BT;\n");
 			// ASM's ClassReader does the heavy lifting of parsing the compiled
 			// Java class
 			ClassReader reader = new ClassReader(className);
 			// make class declaration visitor to get superclass and interfaces
-			ClassVisitor declVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, builder);
-			
-			builder.append(className + " [\nshape=\"record\",\n");
-			builder.append("label = \"[" + className + "|");
+			ClassVisitor declVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, arrowBuilder);
+			String[] classNameToWriteArray = className.split("[.]");
+			String classNameToWrite = classNameToWriteArray[classNameToWriteArray.length-1];
+			completeBuilder.append(classNameToWrite + " [\nshape=\"record\",\n");
+			completeBuilder.append("label = \"{" + className + "|");
 			// DECORATE declaration visitor with field visitor
-			ClassVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5, declVisitor, builder);
+			ClassVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5, declVisitor, fieldBuilder);
 			// DECORATE field visitor with method visitor
-			ClassVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5, fieldVisitor, builder);
+			ClassVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5, fieldVisitor, methodBuilder);
 			// TODO: add more DECORATORS here in later milestones to accomplish
 			// specific tasks
 
 			// Tell the Reader to use our (heavily decorated) ClassVisitor to
 			// visit the class
 			reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
-			builder.append("}\"\n];");
+			completeBuilder.append(fieldBuilder.toString());
+			completeBuilder.append("|");
+			completeBuilder.append(methodBuilder);
+			completeBuilder.append("}\"\n];\n}");
 
 			FileOutputStream writer = new FileOutputStream("output/output.txt");
-			writer.write(builder.toString().getBytes());
+			writer.write(completeBuilder.toString().getBytes());
 		}
 	}
 
