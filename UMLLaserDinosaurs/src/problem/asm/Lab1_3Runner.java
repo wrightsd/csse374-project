c9a408f4010d08322ecc64288ad607ed64feff29
Gregory Nathan <nathangj@rose-hulman.edu>
2015-12-16 22:53:41 -0500

    Tim Anderson: Forgot to include other file in previous commit

---
+++ UMLLaserDinosaurs/src/problem/asm/Lab1_3Runner.java
@@ -0,0 +1,13 @@
+package problem.asm;
+
+import java.io.IOException;
+
+public class Lab1_3Runner {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		String[] arguments = {"problem.AppLauncher", "problem.BackwardsTextPrinter", "problem.FileNamePrinter", "problem.Observer", "problem.Subject", "problem.TextLoader", "problem.WebsiteLoader", "problem.WordLoader"};
+		DesignParser.parse(arguments);
+	}
+
+}
