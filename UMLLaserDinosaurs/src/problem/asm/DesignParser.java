2a583a092145bf1e85604d78bf38b8cdb7b1dfbc
Gregory Nathan <nathangj@rose-hulman.edu>
2015-12-16 22:53:13 -0500

    Tim Anderson: Made for loop for code to loop over multiple classes, made a separate runner to generate output for the lab1-3 files specifically.

--- UMLLaserDinosaurs/src/problem/asm/DesignParser.java
+++ UMLLaserDinosaurs/src/problem/asm/DesignParser.java
@@ -9,21 +9,22 @@ import org.objectweb.asm.Opcodes;
 
 public class DesignParser {
 
-	public static void main(String[] args) throws IOException {
+	public static void parse(String[] args) throws IOException {
+		StringBuilder completeBuilder = new StringBuilder();
+		completeBuilder.append("digraph text{\n");
+		completeBuilder.append("rankdir=BT;\n");
+
 		for (String className : args) {
-			StringBuilder completeBuilder = new StringBuilder();
 			StringBuilder methodBuilder = new StringBuilder();
 			StringBuilder fieldBuilder = new StringBuilder();
 			StringBuilder arrowBuilder = new StringBuilder();
-			completeBuilder.append("digraph text{\n");
-			completeBuilder.append("rankdir=BT;\n");
 			// ASM's ClassReader does the heavy lifting of parsing the compiled
 			// Java class
 			ClassReader reader = new ClassReader(className);
 			// make class declaration visitor to get superclass and interfaces
 			ClassVisitor declVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, arrowBuilder);
 			String[] classNameToWriteArray = className.split("[.]");
-			String classNameToWrite = classNameToWriteArray[classNameToWriteArray.length-1];
+			String classNameToWrite = classNameToWriteArray[classNameToWriteArray.length - 1];
 			completeBuilder.append(classNameToWrite + " [\nshape=\"record\",\n");
 			completeBuilder.append("label = \"{" + className + "|");
 			// DECORATE declaration visitor with field visitor
@@ -36,17 +37,21 @@ public class DesignParser {
 			// Tell the Reader to use our (heavily decorated) ClassVisitor to
 			// visit the class
 			reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
-			completeBuilder.append(fieldBuilder.toString());
-			completeBuilder.append("|");
-			completeBuilder.append(methodBuilder);
+			if (fieldBuilder.toString().length() > 0) {
+				completeBuilder.append(fieldBuilder.toString());
+				completeBuilder.append("|");
+			}
+			if (methodBuilder.toString().length() > 0) {
+				completeBuilder.append(methodBuilder);
+			}
 			completeBuilder.append("}\"\n];\n");
 			completeBuilder.append(arrowBuilder.toString());
-			completeBuilder.append("}");
-
-			FileOutputStream writer = new FileOutputStream("output/output.txt");
-			writer.write(completeBuilder.toString().getBytes());
-			writer.close();
 		}
+
+		completeBuilder.append("}");
+		FileOutputStream writer = new FileOutputStream("output/output.txt");
+		writer.write(completeBuilder.toString().getBytes());
+		writer.close();
 	}
 
 }
