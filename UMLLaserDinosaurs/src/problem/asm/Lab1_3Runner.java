e17db511ec22aba38c264594e70eca071b9817fd
Gregory Nathan <nathangj@rose-hulman.edu>
2016-02-14 23:24:37 -0500

    Gregory: Added functionality to pull all classes from a file

--- UMLLaserDinosaurs/src/problem/asm/Lab1_3Runner.java
+++ UMLLaserDinosaurs/src/problem/asm/Lab1_3Runner.java
@@ -1,14 +1,34 @@
 package problem.asm;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Lab1_3Runner {
 
 	public static void main(String[] args) throws IOException {
-		String[] arguments = { "problem.AppLauncher", "problem.BackwardsTextPrinter", "problem.FileNamePrinter",
-				"problem.Observer", "problem.Subject", "problem.TextLoader", "problem.WebsiteLoader",
-				"problem.WordLoader" };
-		DesignParser.parse(arguments, "./output/lab1-3_output.txt", "uml");
+		File classFolder = new File(
+				"C:/EclipseWorkspaces/csse374/csse374-project/UMLLaserDinosaurs/Lab1-3ClassesToUML");
+		File[] packageArray = classFolder.listFiles();
+		String[] packageArguments = new String[packageArray.length];
+		for(int i = 0; i < packageArray.length; i++){
+			packageArguments[i] = packageArray[i].getName();
+		}
+		ArrayList<String> arguments = new ArrayList<String>();
+		for(int i = 0; i < packageArguments.length; i++){
+			File packageFolder = new File(
+					"C:/EclipseWorkspaces/csse374/csse374-project/UMLLaserDinosaurs/Lab1-3ClassesToUML/" + packageArguments[i]);
+			File[] classArray = packageFolder.listFiles();
+			for(int  j= 0; j < classArray.length; j++){
+				arguments.add(packageArguments[i] + "." + classArray[j].getName().split(".java")[0]);
+			}
+		}
+//		String[] arguments = { "problem.AppLauncher", "problem.BackwardsTextPrinter", "problem.FileNamePrinter",
+//				"problem.Observer", "problem.Subject", "problem.TextLoader", "problem.WebsiteLoader",
+//				"problem.WordLoader" };
+		DesignParser.parse((String[]) arguments.toArray(new String[arguments.size()]), "./output/lab1-3_output.txt", "uml");
+		
 	}
 
 }
