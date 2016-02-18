7d2ba301d78b0ea1c1ebe195a522bbdfa3747dc3
Gregory Nathan <nathangj@rose-hulman.edu>
2016-02-17 22:54:31 -0500

    Gregory: Implemented button to load configuration file from directory

---
+++ UMLLaserDinosaurs/src/problem/asm/MainRunner.java
@@ -0,0 +1,144 @@
+package problem.asm;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MainRunner {
+	
+	static String inputFolder;
+	static ArrayList<String> inputClasses;
+	static String outputDirectory;
+	static String dotPath;
+	static ArrayList<String> patterns;
+	
+	public static void main(String[] args)  {
+		
+		JFrame configWindow = new JFrame();
+		JPanel configLoadPanel = new JPanel();
+		JButton loadButton = new JButton("Load Config File");
+		
+		loadButton.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser configSelector = new JFileChooser();
+				int returnVal = configSelector.showOpenDialog((Component) e.getSource());
+
+		        if (returnVal == JFileChooser.APPROVE_OPTION) {
+		            File file = configSelector.getSelectedFile();
+		            try {
+						processConfig(file);
+					} catch (Exception e1) {
+						e1.printStackTrace();
+					}
+		            
+		        }
+				
+			}
+			
+		});
+		
+		configLoadPanel.add(loadButton);
+		configWindow.add(configLoadPanel);
+		
+		configWindow.pack();
+		configWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		configWindow.setVisible(true);
+	}
+	
+	private static void processConfig(File configFile) throws Exception{
+		parseConfiguration(configFile.getPath());
+		if (patterns == null || dotPath == null || (inputClasses == null && inputFolder == null)
+				|| outputDirectory == null) {
+			System.out.println("Configuration incorrect.");
+			return;
+		}
+
+		String inputFilePath = outputDirectory + "output.txt";
+		String outputFilePath = outputDirectory + "output.png";
+
+		ArrayList<String> arguments = new ArrayList<String>();
+
+		if (inputFolder != null) {
+			System.out.println(inputFolder);
+			File classFolder = new File(inputFolder);
+			File[] packageArray = classFolder.listFiles();
+			String[] packageArguments = new String[packageArray.length];
+			for (int i = 0; i < packageArray.length; i++) {
+				packageArguments[i] = packageArray[i].getName();
+			}
+			for (int i = 0; i < packageArguments.length; i++) {
+				File packageFolder = new File(inputFolder + "\\" + packageArguments[i]);
+				File[] classArray = packageFolder.listFiles();
+				for (int j = 0; j < classArray.length; j++) {
+					arguments.add(packageArguments[i] + "." + classArray[j].getName().split(".java")[0]);
+				}
+			}
+		}
+		if (inputClasses != null) {
+			arguments.addAll(inputClasses);
+		}
+		// String[] arguments = { "problem.AppLauncher",
+		// "problem.BackwardsTextPrinter", "problem.FileNamePrinter",
+		// "problem.Observer", "problem.Subject", "problem.TextLoader",
+		// "problem.WebsiteLoader",
+		// "problem.WordLoader" };
+		DesignParser.parse((String[]) arguments.toArray(new String[arguments.size()]), inputFilePath, "uml");
+
+		Runtime runTimeEnvironment = Runtime.getRuntime();
+		Process showingProcess = runTimeEnvironment
+				.exec("cmd /c \"" + dotPath + "\" -Tpng " + inputFilePath + " > " + outputFilePath);
+	}
+	
+	private static void parseConfiguration(String fileName) throws Exception {
+		String line;
+		FileInputStream file = new FileInputStream(fileName);
+		InputStreamReader r = new InputStreamReader(file);
+		BufferedReader br = new BufferedReader(r);
+		while ((line = br.readLine()) != null) {
+			if (line.startsWith("input-folder")) {
+				if (line.length() > 13) {
+					inputFolder = line.substring(13);
+				}
+			} else if (line.startsWith("input-classes")) {
+				if (line.length() > 14) {
+					inputClasses = new ArrayList<String>();
+					for (String s : line.substring(14).split(",")) {
+						inputClasses.add(s);
+					}
+				}
+			} else if (line.startsWith("output-directory")) {
+				if (line.length() > 17) {
+					outputDirectory = line.substring(17);
+				}
+			} else if (line.startsWith("dot-path")) {
+				dotPath = line.substring(9);
+			} else if (line.startsWith("patterns")) {
+				if (line.length() > 9) {
+					patterns = new ArrayList<String>();
+					for (String s : line.substring(9).split(",")) {
+						patterns.add(s);
+					}
+				}
+			} else {
+				//System.out.println(line);
+				throw new UnsupportedOperationException();
+			}
+		}
+		file.close();
+	}
+
+}
