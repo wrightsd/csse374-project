81e264afdd23e3bee144bce069e7b07bef29eb1d
QuantumMenace <tmthyndrsn@gmail.com>
2016-02-18 01:03:24 -0500

    Tim Anderson: GUI now displays checkboxes for each pattern selected from config files and displays the loaded png file of the UML diagram

--- UMLLaserDinosaurs/src/problem/asm/MainRunner.java
+++ UMLLaserDinosaurs/src/problem/asm/MainRunner.java
@@ -1,17 +1,23 @@
 package problem.asm;
 
+import java.awt.Checkbox;
 import java.awt.Component;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 
+import javax.imageio.ImageIO;
+import javax.imageio.stream.FileImageInputStream;
+import javax.imageio.stream.ImageInputStream;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -20,63 +26,97 @@ import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 
 public class MainRunner {
-	
+
 	static String inputFolder;
 	static ArrayList<String> inputClasses;
 	static String outputDirectory;
 	static String dotPath;
 	static ArrayList<String> patterns;
-	
+
 	static JFrame configWindowField;
 	static JPanel configLoadPanelField;
 	static JButton loadButtonField;
-	
-	public static void main(String[] args)  {
-		
+
+	public static void main(String[] args) {
+
 		JFrame configWindow = new JFrame();
 		configWindowField = configWindow;
 		JPanel configLoadPanel = new JPanel();
 		configLoadPanelField = configLoadPanel;
 		JButton loadButton = new JButton("Load Config File");
 		loadButtonField = loadButton;
-		
-		loadButton.addActionListener(new ActionListener(){
+
+		loadButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JFileChooser configSelector = new JFileChooser();
 				int returnVal = configSelector.showOpenDialog((Component) e.getSource());
 
-		        if (returnVal == JFileChooser.APPROVE_OPTION) {
-		            File file = configSelector.getSelectedFile();
-		            try {
-						processConfig(file,loadButton);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File file = configSelector.getSelectedFile();
+					try {
+						processConfig(file, loadButton);
 					} catch (Exception e1) {
 						e1.printStackTrace();
 					}
-		            
-		        }
-				
+
+				}
+
 			}
-			
+
 		});
-		
+
 		configLoadPanel.add(loadButton);
 		configWindow.add(configLoadPanel);
-		
+
 		configWindow.pack();
 		configWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		configWindow.setVisible(true);
 	}
-	
-	private static void displayResults() {
-		// TODO Auto-generated method stub
-		
+
+	private static void displayResults() throws Exception {
+		configLoadPanelField.removeAll();
+		configLoadPanelField.setLayout(new BoxLayout(configLoadPanelField, BoxLayout.X_AXIS));
+		JPanel checkboxes = new JPanel();
+		checkboxes.setLayout(new BoxLayout(checkboxes, BoxLayout.Y_AXIS));
+		for (int i = 0; i < patterns.size(); i++) {
+			Checkbox cb = new Checkbox(patterns.get(i));
+			checkboxes.add(cb);
+		}
+		configLoadPanelField.add(checkboxes);
+		//// JPanel image = new JPanel();
+		// BufferedImage bImage;
+		// File imageLocation = new File(outputDirectory + "output.png");
+		// ImageInputStream iis = new FileImageInputStream(imageLocation);
+		// bImage = ImageIO.read(iis);
+		// JLabel diagram = new JLabel(new ImageIcon(bImage));
+		boolean fileNotLoaded = true;
+		String name = outputDirectory + "output.png";
+		name = name.replace('\\', '/');
+		ImageIcon icon;
+		while (fileNotLoaded) {
+			try {
+				File f = new File(name);
+				if (f != null) {
+					fileNotLoaded = false;
+				}
+			} catch (Exception e) {
+				fileNotLoaded = true;
+			}
+		}
+
+		icon = new ImageIcon(name);
+		JLabel diagram = new JLabel();
+		diagram.setIcon(icon);
+		configLoadPanelField.add(diagram);
+		configWindowField.pack();
+		configLoadPanelField.setVisible(true);
 	}
 
-	private static JProgressBar displayLoadingBar(JButton loadButton,String fileName) {
+	private static JProgressBar displayLoadingBar(JButton loadButton, String fileName) {
 		JProgressBar bar = new JProgressBar();
-		JLabel nameOfFile = new JLabel("Loading: "+fileName);
+		JLabel nameOfFile = new JLabel("Loading: " + fileName);
 		configLoadPanelField.remove(loadButton);
 		configLoadPanelField.setLayout(new BoxLayout(configLoadPanelField, BoxLayout.Y_AXIS));
 		configLoadPanelField.add(nameOfFile);
@@ -84,15 +124,14 @@ public class MainRunner {
 		configWindowField.pack();
 		configLoadPanelField.setVisible(true);
 		return bar;
-		
-		
+
 	}
-	
-	private static void processConfig(File configFile, JButton loadButton) throws Exception{
+
+	private static void processConfig(File configFile, JButton loadButton) throws Exception {
 		JProgressBar bar = displayLoadingBar(loadButton, configFile.getName());
-		
-		updateLoadBar(bar,25);
-		
+
+		updateLoadBar(bar, 25);
+
 		parseConfiguration(configFile.getPath());
 		if (patterns == null || dotPath == null || (inputClasses == null && inputFolder == null)
 				|| outputDirectory == null) {
@@ -133,11 +172,14 @@ public class MainRunner {
 		Runtime runTimeEnvironment = Runtime.getRuntime();
 		Process showingProcess = runTimeEnvironment
 				.exec("cmd /c \"" + dotPath + "\" -Tpng " + inputFilePath + " > " + outputFilePath);
+
+		showingProcess.waitFor();
+		displayResults();
 	}
-	
+
 	private static void updateLoadBar(JProgressBar bar, int value) {
 		bar.setValue(value);
-		
+
 	}
 
 	private static void parseConfiguration(String fileName) throws Exception {
@@ -171,11 +213,14 @@ public class MainRunner {
 					}
 				}
 			} else {
-				//System.out.println(line);
+				// System.out.println(line);
+				file.close();
+				br.close();
 				throw new UnsupportedOperationException();
 			}
 		}
 		file.close();
+		br.close();
 	}
 
 }
